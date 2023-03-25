3e81252c815c84279449f970fd89255e404f8e7a
b4-tech <hcetanutrof@gmail.com>
2023-03-24 21:40:54 -0400

    feat: migrate post call to api route handler

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -3,17 +3,22 @@ import EmptyState from "@/app/common/components/layout/EmptyState";
 import getCurrentUser from "@/app/common/actions/getCurrentUser";
 import getFavoriteListings from "@/app/common/actions/getFavoriteListings";
 
-import FavoritesView from "./FavoritesView";
+import FavoritesClient from "./FavoritesClient";
 
 const ListingPage = async () => {
   const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return <EmptyState title="Unauthorized" subtitle="Please login" />;
+  }
+
   const listings = await getFavoriteListings({ currentUser });
 
   if (!listings) {
     return <EmptyState />;
   }
 
-  return <FavoritesView listings={listings} currentUser={currentUser} />;
+  return <FavoritesClient listings={listings} currentUser={currentUser} />;
 };
 
 export default ListingPage;
