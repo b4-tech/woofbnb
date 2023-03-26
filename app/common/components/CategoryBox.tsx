6b9d32237b616cd04848f8fd494a4962e3c11d8c
b4-tech <hcetanutrof@gmail.com>
2023-03-26 19:16:43 -0400

    feat: cleanup

--- app/common/components/CategoryBox.tsx
+++ app/common/components/CategoryBox.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
 import { useRouter, useSearchParams } from "next/navigation";
-import qs from "query-string";
+import qs from 'query-string';
 import { useCallback } from "react";
 import { IconType } from "react-icons";
 
 interface CategoryBoxProps {
-  icon: IconType;
+  icon: IconType,
   label: string;
   selected?: boolean;
 }
@@ -20,32 +20,30 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
   const params = useSearchParams();
 
   const handleClick = useCallback(() => {
-    const newQuery: any = {
-      locationValue: params?.get("locationValue"),
-      guestCount: params?.get("guestCount"),
-      roomCount: params?.get("roomCount"),
-      bathroomCount: params?.get("bathroomCount"),
-      startDate: params?.get("startDate"),
-      endDate: params?.get("endDate"),
-      category: label,
-    };
-
-    if (params?.get("category") === label) {
-      delete newQuery.category;
+    let currentQuery = {};
+    
+    if (params) {
+      currentQuery = qs.parse(params.toString())
     }
 
-    const url = qs.stringifyUrl(
-      {
-        url: "/",
-        query: newQuery,
-      },
-      { skipNull: true }
-    );
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label
+    }
+
+    if (params?.get('category') === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl({
+      url: '/',
+      query: updatedQuery
+    }, { skipNull: true });
 
     router.push(url);
   }, [label, router, params]);
 
-  return (
+  return ( 
     <div
       onClick={handleClick}
       className={`
@@ -59,14 +57,16 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
         hover:text-neutral-800
         transition
         cursor-pointer
-        ${selected ? "border-b-neutral-800" : "border-transparent"}
-        ${selected ? "text-neutral-800" : "text-neutral-500"}
+        ${selected ? 'border-b-neutral-800' : 'border-transparent'}
+        ${selected ? 'text-neutral-800' : 'text-neutral-500'}
       `}
     >
       <Icon size={26} />
-      <div className="font-medium text-sm">{label}</div>
+      <div className="font-medium text-sm">
+        {label}
+      </div>
     </div>
-  );
-};
-
+   );
+}
+ 
 export default CategoryBox;
